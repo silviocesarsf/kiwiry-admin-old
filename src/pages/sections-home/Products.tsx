e823fdfce2f14737a97fda75fc4214a3954c0a74
silvio <silvio14dmc@gmail.com>
2025-05-15 06:35:53 -0300

    initialized backend

--- src/pages/sections-home/Products.tsx
+++ src/pages/sections-home/Products.tsx
@@ -1,5 +1,5 @@
-import { Button, Input, Menu, ScrollArea, Table } from "@mantine/core";
-import { EllipsisVertical, SearchIcon } from "lucide-react";
+import { Button, Input, Menu, ScrollArea, Select, Table } from "@mantine/core";
+import { Edit, EllipsisVertical, SearchIcon, Trash } from "lucide-react";
 import RegisterProductModal from "../../components/modal/RegisterProductModal";
 import { useDisclosure } from "@mantine/hooks";
 import "../../styles/Products.css";
@@ -15,50 +15,39 @@ export default function Products() {
                 <Input placeholder="Pesquisar" className="flex-1" leftSection={<SearchIcon size={"1.4rem"} />} />
                 <Button onClick={open}>Adicionar Produto</Button>
             </div>
-            <ScrollArea.Autosize h={"85%"}>
-                <Table highlightOnHover striped verticalSpacing={"lg"} withTableBorder>
+            <Table.ScrollContainer minWidth={500} maxHeight={800}>
+                <Table verticalSpacing={"lg"} striped highlightOnHover stickyHeader stickyHeaderOffset={0}>
                     <Table.Thead>
                         <Table.Tr>
                             <Table.Th>ID</Table.Th>
-                            <Table.Th>Foto</Table.Th>
                             <Table.Th>Nome</Table.Th>
-                            <Table.Th>Descrição</Table.Th>
-                            <Table.Th>Valor</Table.Th>
+                            <Table.Th>Descrição</Table.Th>
+                            <Table.Th>Preço</Table.Th>
+                            <Table.Th>Editar</Table.Th>
+                            <Table.Th>Excluir</Table.Th>
                         </Table.Tr>
                     </Table.Thead>
                     <Table.Tbody>
                         <Table.Tr>
-                            <Table.Td>#88928</Table.Td>
+                            <Table.Td>#1</Table.Td>
+                            <Table.Td>Pão de açucar</Table.Td>
+                            <Table.Td>Pãozinho assado no fogo de chão</Table.Td>
+                            <Table.Td>R$ 1,00</Table.Td>
                             <Table.Td>
-                                <div className="overflow-hidden rounded-md w-16 h-16">
-                                    <img className="w-full h-full object-cover" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
-                                </div>
+                                <span className="cursor-pointer"><Edit /></span>
                             </Table.Td>
-                            <Table.Td>Teste</Table.Td>
-                            <Table.Td>Descrição akii HAHAHHAHHAAHAHAHAHAHHAHHAHAHHAH</Table.Td>
-                            <Table.Td>R$ 92,90</Table.Td>
                             <Table.Td>
-                                <Menu>
-                                    <Menu.Target>
-                                        <div className="cursor-pointer">
-                                            <EllipsisVertical size={"1.4rem"} />
-                                        </div>
-                                    </Menu.Target>
-                                    <Menu.Dropdown>
-                                        <Menu.Item>Editar</Menu.Item>
-                                        <Menu.Item>Excluir</Menu.Item>
-                                    </Menu.Dropdown>
-                                </Menu>
+                                <span className="cursor-pointer"><Trash /></span>
                             </Table.Td>
                         </Table.Tr>
                     </Table.Tbody>
                     <Table.Tfoot>
                         <Table.Tr>
-                            <Table.Td colSpan={4} className="text-gray-500">Exibindo 1 de 1 Produtos</Table.Td>
+                            <Table.Td colSpan={6}>Exibindo 10 de 100 registros</Table.Td>
                         </Table.Tr>
                     </Table.Tfoot>
                 </Table>
-            </ScrollArea.Autosize>
+            </Table.ScrollContainer>
         </div>
     )
 }
